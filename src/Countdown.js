7804eb8d253ee68f0744b5afd74e2fca480b9795
Elankumaran98 <siva98kumarane@gmail.com>
2022-01-06 15:13:04 +0530

    first commit

---
+++ src/Countdown.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Countdown = ({ name, day, month, countdownData }) => {
+  const presentDate = new Date();
+  if (
+    presentDate.getDate() === parseInt(day) &&
+    presentDate.getMonth() === parseInt(month) - 1
+  ) {
+    return (
+      <div className="wish">
+        <p>
+          It's time to Give Gift <span className="name">{name}</span>'.
+        </p>
+        <p>
+          <strong>
+            Wish <span className="name">{name}</span>
+          </strong>
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="countdown-wrapper">
+          <div className="countdown-box">
+            {countdownData.days}
+            <span className="legend">Days</span>
+          </div>
+          <div className="countdown-box">
+            {countdownData.hours}
+            <span className="legend">Hrs</span>
+          </div>
+          <div className="countdown-box">
+            {countdownData.minutes}
+            <span className="legend">Mins</span>
+          </div>
+          <div className="countdown-box">
+            {countdownData.seconds}
+            <span className="legend">Sec</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Countdown;
---
+++ src/List.js
@@ -0,0 +1,62 @@
+import React from "react";
+import moment from "moment";
+import Birthday from "./ReminderDate";
+import { FaEdit, FaTrash } from "react-icons/fa";
+
+const List = ({ people, removePerson, editPerson, today, setAmount }) => {
+  let filteredData = people.filter((person) => {
+    const presentDate = new Date();
+
+    return (
+      presentDate.getDate() === parseInt(moment(person.date).format("D")) &&
+      presentDate.getMonth() === parseInt(moment(person.date).format("M")) - 1
+    );
+  });
+  setAmount(filteredData.length);
+  const birthday = today === true ? filteredData : people;
+  return (
+    <>
+      {today === true && birthday.length === 0 ? (
+        <div className="no-birthday-today">No Reminds Today</div>
+      ) : (
+        birthday.map((person) => {
+          const { id, name, age, date, image } = person;
+          return (
+            <article key={id} className="person">
+              <img
+                src={image}
+                alt={name}
+                onError={(e) =>
+                  (e.target.src = `https://avatars.dicebear.com/api/initials/${name}.svg`)
+                }
+              />
+              <div className="pesron-info">
+                <h4>{name}</h4>
+                <p>{moment(date).format("MMM Do YYYY")}</p>
+                <p>{age}</p>
+                <Birthday
+                  name={name}
+                  age={age}
+                  day={moment(date).format("D")}
+                  month={moment(date).format("M")}
+                />
+              </div>
+              <div className="btns" aria-label="Edit">
+                <button className="btn" onClick={() => editPerson(id)}>
+                  <a href="#edit">
+                    <FaEdit size="20" />
+                  </a>
+                </button>
+                <button className="btn" aria-label="Delete">
+                  <FaTrash size="20" onClick={() => removePerson(id)} />
+                </button>
+              </div>
+            </article>
+          );
+        })
+      )}
+    </>
+  );
+};
+
+export default List;
